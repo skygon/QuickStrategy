aacbda3ba03e7c455c55287f7b960c8ec7886ae2
skygon <skygoncui@163.com>
2017-05-21 21:13:12 +0800

    update

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 # -*- coding: utf8 -*-
 import os
 
-f = open('E:\sh603993.csv')
+f = open('E:\skygon\QuickStrategy\detail_data\sh600010.csv')
+
+count = 0
 
 line = f.readline()
 while line:
     print line
+    print line.strip('\n').split('\t')
     line = f.readline()
+    count += 1
+    if count > 10:
+        break
 
 f.close()
