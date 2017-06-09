e41b96c5140622d4a66d2222b8e5c988b1b4ff26
skygon <skygoncui@163.com>
2017-06-09 23:11:39 +0800

    update

--- trainning.py
+++ trainning.py
@@ -3,17 +3,24 @@ import operator
 from utils import *
 from RedisOperator import RedisOperator
 
-# get redis connection
-r = RedisOperator('localhost',6379,0)
-# sh small rank
-sh_small = {} # code <-> changepercent
 
-for k in code_vol_map['sh']['small'].keys():
-    s = r.hget('index_4_dict', k)
-    if s is None:
-        continue
-    data = json.loads(s)
-    sh_small[k] = float(data['changepercent'])
+class Trainning(object):
+    def __init__(self):
+        # get redis connection
+        self.redis= RedisOperator('localhost',6379,0)
+        # sh small rank
+        self.sh_small = {} # code <-> changepercent
+        self.getTrainningCode()
+    
+    def getTrainningCode(self):
+        for k in code_vol_map['sh']['small'].keys():
+            s = self.redis.hget('index_4_dict', k)
+            if s is None:
+                continue
+            data = json.loads(s)
+            self.sh_small[k] = float(data['changepercent'])
 
-ranked =  sorted(sh_small.items(), key=operator.itemgetter(1), reverse=True)
-print ranked
+            #ranked =  sorted(sh_small.items(), key=operator.itemgetter(1), reverse=True)
+            #print ranked
+
+    
