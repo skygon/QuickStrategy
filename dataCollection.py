5635959d4e87a43f12269b757cc226a52f5e7225
skygon <skygoncui@163.com>
2017-06-22 21:45:23 +0800

    update

--- dataCollection.py
+++ dataCollection.py
@@ -130,7 +130,7 @@ class DataCollection(threading.Thread):
 def getBillDetail(day_index, date_string):
     # 5 levels
     tdc = []
-    level = 0
+    level = 4
     print "================================================== \n"
     print "Start Level %s\n" %(level)
     for i in range(dc_thread_poll):
@@ -147,5 +147,5 @@ def getIndex(day_index):
 
 
 if __name__ == "__main__":
-    getBillDetail(12, "2017-06-21")
-    #getIndex(12)
+    getBillDetail(13, "2017-06-22")
+    #getIndex(13)
