d7d54272b4075f71ffc119636d78e1bca1a9befc
yui <ycui@microstrategy.com>
2017-06-23 17:26:07 +0800

    update

--- dataCollection.py
+++ dataCollection.py
@@ -130,7 +130,7 @@ class DataCollection(threading.Thread):
 def getBillDetail(day_index, date_string):
     # 5 levels
     tdc = []
-    level = 4
+    level = 0
     print "================================================== \n"
     print "Start Level %s\n" %(level)
     for i in range(dc_thread_poll):
@@ -147,5 +147,5 @@ def getIndex(day_index):
 
 
 if __name__ == "__main__":
-    getBillDetail(13, "2017-06-22")
-    #getIndex(13)
+    getBillDetail(14, "2017-06-23")
+    #getIndex(14)
